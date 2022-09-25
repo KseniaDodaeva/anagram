eec51d51f3ccc258ffa14617b51fd15647db6a0c
KseniaDodaeva <66111848+KseniaDodaeva@users.noreply.github.com>
2022-09-26 00:24:40 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace pr_12._09._22
+{
+    class Program
+    {
+        public static string isAnagramm(string str1, string str2)
+        {
+            string pattern = @"\.+?|\,+?|\ +?|\-+?|\:+?|\!+?";
+            str1 = (Regex.Replace(str1, pattern, String.Empty));
+            str2 = (Regex.Replace(str2, pattern,String.Empty));
+            char[] a = ((str1.ToLower()).ToCharArray());
+            char[] b = ((str2.ToLower()).ToCharArray());
+            Array.Sort(a);
+            Array.Sort(b);
+            string x = new string(a);
+            string y = new string(b);
+            if (Equals(x, y)) return "Анаграмма";
+            else return "Не анаграмма";
+        }
+
+        static void Main(string[] args)
+        {
+            string input1 = Console.ReadLine();
+            string input2 = Console.ReadLine();
+            Console.Write(isAnagramm(input1, input2));
+        }
+    }
+}
